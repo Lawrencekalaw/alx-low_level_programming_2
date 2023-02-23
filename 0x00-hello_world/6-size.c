f778a3c1911cdd68fb1a8c11d46c8c93b67ad1d3
Lawrencekalaw <lawrencemaina272@gmail.com>
2023-02-23 07:41:52 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main-sizes of a computer
- * Return:0(Success)
+ * main-A program to output size of text
+ * Return:0 (Success)
  */
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of a int: %d byte(s)\n",sizeof(b));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d byte(s)\n", sizeof(e));
